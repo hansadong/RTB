9cf5aed099ce6a3b791a539afe4891e7b560c90c
hansadong <jhhan1108@gmail.com>
2024-09-14 21:08:51 +0900

    repository update

--- src/test/java/com/toy/rtb/RtbApplicationTests.java
+++ src/test/java/com/toy/rtb/RtbApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.toy.rtb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RtbApplicationTests {
 
     @Test
     void contextLoads() {
