9deb175a4bf72edabfd59baffcf682a21578a850
hansadong <jhhan1108@gmail.com>
2024-10-14 12:37:42 +0900

    오류 수정

--- src/main/java/com/toy/rtb/config/SecurityConfiguration.java
+++ src/main/java/com/toy/rtb/config/SecurityConfiguration.java
@@ -9,12 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -40,26 +36,26 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                // CSRF 비활성화
-                .csrf().disable()
-
-                // 예외 처리
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-
-                // 세션을 사용하지 않음
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-
-                // 권한 설정
-                .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated();
-
-        // JWT 필터 추가
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                // CSRF 비활성화
+//                .csrf().disable()
+//
+//                // 예외 처리
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//
+//                // 세션을 사용하지 않음
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//
+//                // 권한 설정
+//                .authorizeHttpRequests()
+//                .requestMatchers("/api/auth/**").permitAll()
+//                .anyRequest().authenticated();
+//
+//        // JWT 필터 추가
+//        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
 }
