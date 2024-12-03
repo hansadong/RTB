3cce751741151253218a28a3a807ddca21c33e5f
hansadong <jhhan1108@gmail.com>
2024-12-04 01:32:59 +0900

    RecipeIngredients 엔터티 복합키 설정 추가

--- src/main/java/com/toy/rtb/model/recipe/RecipeIngredients.java
+++ src/main/java/com/toy/rtb/model/recipe/RecipeIngredients.java
@@ -5,10 +5,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 @Entity
 @Table(name = "recipe_ingredients")
 @Getter
 @Setter
+@IdClass(RecipeIngredients.CompositeKey.class)
 public class RecipeIngredients extends CommonEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +30,26 @@ public class RecipeIngredients extends CommonEntity {
 
     @Column(nullable = false)
     private String cookingTime;
+
+    public static class CompositeKey implements Serializable {
+        private Long recipeSn;
+        private Long ingredientSn;
+
+        // 기본 생성자, getter, setter, equals(), hashCode() 구현
+
+        public CompositeKey() {}
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CompositeKey that = (CompositeKey) o;
+            return Objects.equals(recipeSn, that.recipeSn) && Objects.equals(ingredientSn, that.ingredientSn);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(recipeSn, ingredientSn);
+        }
+    }
 }
