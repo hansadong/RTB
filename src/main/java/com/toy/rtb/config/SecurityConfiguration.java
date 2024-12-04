b7cdc40d318041f3b69477deb370d548e5a08b51
hansadong <jhhan1108@gmail.com>
2024-12-04 23:53:37 +0900

    security config 수정

--- src/main/java/com/toy/rtb/config/SecurityConfiguration.java
+++ src/main/java/com/toy/rtb/config/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -46,7 +45,7 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 // 권한 설정
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() // 인증 요청만 허용
+                        .requestMatchers("/api/**/**").permitAll() // 인증 요청만 허용
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 );
 
